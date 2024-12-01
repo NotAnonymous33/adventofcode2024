e89d184ac943e6c6cd0f2934ee0a8673d14c2cb9
k71796ic <ismail.choudhury@student.manchester.ac.uk>
2024-12-01 18:58:47 +0000

    day1 c++

---
+++ day1/day1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::ifstream file("day1input.txt");
+    int sum = 0;
+    int num1, num2;
+    std::vector<int> list1, list2;
+
+    while (file >> num1 >> num2) {
+        list1.push_back(num1);
+        list2.push_back(num2);
+    }
+    sort(list1.begin(), list1.end());
+    sort(list2.begin(), list2.end());
+
+    for (size_t i = 0; i < list1.size(); i++) {
+        sum += abs(list1[i] - list2[i]);
+    }
+
+    std::cout << sum << std::endl;
+    return 0;
+}
