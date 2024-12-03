258bc766cb5b33f348c9147dfc223f235181e7cc
k71796ic <ismail.choudhury@student.manchester.ac.uk>
2024-12-03 10:57:40 +0000

    day3 c++

---
+++ day3/day3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+
+
+int main()
+{
+    std::ifstream file("input.txt");
+    std::string input;
+    std::string line;
+    int result = 0;
+    while (std::getline(file, line))
+    {
+        input += line;
+    }
+
+    std::regex pattern(R"(mul\([0-9]+,[0-9]+\))");
+    std::smatch match;
+
+    std::regex_search(input, match, pattern);
+    
+    auto words_begin = std::sregex_iterator(input.begin(), input.end(), pattern);
+    auto words_end = std::sregex_iterator();
+
+
+    for (auto i = words_begin; i != words_end; ++i)
+    {
+        std::smatch match = *i;
+        std::string match_str = match.str();
+        int a,b;
+        sscanf(match_str.c_str(), "mul(%d,%d)", &a, &b);
+        result += a * b;
+    }
+    std::cout << result << std::endl;
+
+
+    
+
+
+
+}
